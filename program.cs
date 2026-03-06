93ebdabb0cbc1d1c470e41a68b093c15a748d84c
AnkitaHeralagi <126943633+AnkitaHeralagi@users.noreply.github.com>
2026-03-06 16:00:19 +0530

    Refactor addition

--- program.cs
+++ program.cs
@@ -2,23 +2,29 @@ using System;
 
 class Program
 {
-    public class Addition{
+    public class Addition
+    {
         public int DoAddition(int a, int b)
         {
-            return a + b;// adding to numbers
+            return a + b; 
         }
-    public class Subtraction{
-        public int DoSubtraction(int a, int b){
+    }
+
+    public class Subtraction
+    {
+        public int DoSubtraction(int a, int b)
+        {
             return a - b;
         }
     }
+
     static void Main(string[] args)
     {
         Addition add = new Addition();
-        add.DoAddition(2,3);
+        Console.WriteLine(add.DoAddition(2,3));
 
         Subtraction sub = new Subtraction();
-        sub.DoSubtraction(5,2);
+        Console.WriteLine(sub.DoSubtraction(5,2));
 
         Multiplication mul = new Multiplication();
         mul.DoMultiplication(2,3);
@@ -32,4 +38,3 @@ class Multiplication
         Console.WriteLine(a*b);
     }
 }
-
